11920b6be27ad226f82ca16a91c44dcd70b36940
Your Name <mkerr3@luc.edu>
2023-12-06 22:00:36 +0000

    first commit

--- Main.sh
+++ Main.sh
@@ -1,5 +1,25 @@
 ferrari
+	carlos 
+	charles
 Mclaren
+	Lando
+	oscar 
 Alphine
+	oscon 
+	pierre Gasly
 Alpha Taruri
+	daniel
+	Yuki
+	LIAM LAWSON
 Williams 
+	LOGAN SARGAENT
+	ALEX ALBON 
+mercedes
+	lewis hamilton
+	george russell
+
+haas
+aston martin
+	lance stroll
+	alonso
+alpha romro
